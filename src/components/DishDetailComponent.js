92287fdbe0d47cb697af146bc3580ea59829211e
Anna-Lissa Sayers <annalissa@gmail.com>
2021-06-12 16:02:10 +1000

    Assignment 1 - Return empty divs on null comments

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -24,22 +24,24 @@ class DishDetail extends Component {
     }
 
     renderComments(dishComments){
-        return dishComments && (
-            <div  className="col-12 col-md-5 m-1">
-                <h4>Comments</h4>
-                
-                    {dishComments.map((value, index) => {  
-                        const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-                        const date =  new Date(value.date);
-                        return (
-                            <ul className="list-unstyled" key={value.id}>
-                                <li>{value.comment}</li>
-                                <li> -- {value.author} { value.date && `, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}` } </li>
-                            </ul>
-                        )
-                    })} 
-            </div>
-        );
+        if (dishComments) {
+            return (
+                <div  className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    
+                        {dishComments.map((value, index) => {  
+                            const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                            const date =  new Date(value.date);
+                            return (
+                                <ul className="list-unstyled" key={value.id}>
+                                    <li>{value.comment}</li>
+                                    <li> -- {value.author} { value.date && `, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}` } </li>
+                                </ul>
+                            )
+                        })} 
+                </div>
+            );
+        }
         return (
             <div></div>
         );
